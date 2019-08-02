0dd53dd0bb90a849e8e53f4770491ce5a7d1957a
shutsugan <shutsugan@gmail.com>
2019-08-02 17:02:34 +0100

    chore (app): move the app file
    move app to the components folder
    wrapp the app with the store provider

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useStoreActions } from 'easy-peasy';
+
+import TodosList from '../TodosList';
 
 import './index.css';
 
 const App = _ => {
+  const initialiseTodos = useStoreActions(actions => actions.initialiseTodos);
+
+  useEffect(_ => {
+    initialiseTodos();
+  }, [initialiseTodos]);
 
   return (
-    <div className="app">the app</div>
+    <div className="app">
+      <TodosList />
+    </div>
   );
 };
 
