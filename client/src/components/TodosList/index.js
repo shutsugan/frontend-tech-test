3e0d808bb6b76eeed8d8e9f826f8d7199694cea5
shutsugan <muhammad@nextmedia.ma>
2019-08-03 18:20:41 +0100

    chore (todoslist): get todos list
    iterate throught the list
    and display each todo

--- client/src/components/TodosList/index.js
+++ client/src/components/TodosList/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
-
 import { useStoreState } from 'easy-peasy';
+
+import Todo from '../Todo';
 
 const TodoList = _ => {
     const todos = useStoreState(state => state.todos.list);
+    const list = todos.map(todo => <Todo key={todo.id} {...todo} />);
     
-    return (
-        <div className="todo-list">
-            todo list
-        </div>
-    );
+    return <div className="todo-list card full flex-column mrb-16">{ list }</div>;
 };
 
 export default TodoList;
