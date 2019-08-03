c809e575739f59a92831b565f41670b97a210bb9
shutsugan <muhammad@nextmedia.ma>
2019-08-03 18:22:18 +0100

    feat (todos model): create adding fonctionality
    create a save method to add and update todos

--- client/src/model/todos.js
+++ client/src/model/todos.js
@@ -1,20 +1,24 @@
 import { action, computed, thunk } from "easy-peasy";
 import axios from 'axios';
 
+const base_url = 'http://localhost:9001';
+
+const save = async (actions, method, url, callback) => {
+    const res = await axios[method](`${base_url}/task/${url}`);
+    const { task, message } = await res.data;
+
+    callback(task);
+    actions.setMessage(message);
+};
+
 export default {
-    todos: [
-        { id: 1, title: 'Work', description: 'some work to do' },
-        { id: 2, title: 'Train', description: 'some training to do' }
-    ],
+    todos: [],
+    message: '',
 
     list: computed(state => Object.values(state.todos)),
 
-    fetchTodos: thunk(async actions => {
-        const res = await axios.get(`http://localhost:9001/tasks`);
-        const { tasks } = await res.data;
-
-        actions.fetched(tasks);
-    }),
+    setMessage: action((state, message) => state.message = message),
+    added: action((state, todo) => state.todos[todo.id] = todo),
 
     fetched: action((state, payload = []) => {
         state.todos = payload.reduce((acc, todo) => {
@@ -22,5 +26,34 @@ export default {
 
             return acc;
         }, {});
+    }),
+
+    fetchTodos: thunk(async actions => {
+        const res = await axios.get(`${base_url}/tasks`);
+        const { tasks } = await res.data;
+
+        actions.fetched(tasks);
+    }),
+
+    addTodo: thunk(async (actions, { id, title, description }) => {
+        if (!title && !description) {
+            return actions.setMessage('Fields are reuqired');
+        }
+
+        if (id) {
+            save(
+                actions,
+                'put',
+                `update/${id}/${title}/${description}`,
+                todo => actions.updated(todo)
+            );
+        } else {
+            save(
+                actions,
+                'post',
+                `create/${title}/${description}`,
+                todo => actions.added(todo)
+            );
+        }
     })
 };
