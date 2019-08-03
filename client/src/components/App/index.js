fefd77798b40ab5cec52e70c69bb5888d44c3432
shutsugan <muhammad@nextmedia.ma>
2019-08-03 18:18:46 +0100

    chore (app): add the addtodo component

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useStoreActions } from 'easy-peasy';
 
 import TodosList from '../TodosList';
+import AddTodo from '../AddTodo';
 
 import './index.css';
 
@@ -13,8 +14,9 @@ const App = _ => {
   }, [initialiseTodos]);
 
   return (
-    <div className="app">
+    <div className="app half mr-auto flex-column center pd-16">
       <TodosList />
+      <AddTodo />
     </div>
   );
 };
