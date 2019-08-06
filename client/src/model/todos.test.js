1c23cdc0b85d52459fcc4d768b12f992221557c1
shutsugan <muhammad@nextmedia.ma>
2019-08-06 23:11:28 +0100

    feat (todos test): finishe testing togos model

--- client/src/model/todos.test.js
+++ client/src/model/todos.test.js
@@ -1,11 +1,13 @@
 import { createStore } from 'easy-peasy';
 import mockAxios from 'jest-mock-axios';
 
-import model from './todos';
+import model, { save, checkProps } from './todos';
 
 const setup = _ => createStore(model);
 const message = 'test message';
 const todo = { id: 0, title: 'test todo title', description: 'test todo description' };
+
+afterEach(() => mockAxios.reset());
 
 //actions
 test('Should return the fetched data', () => {
@@ -36,11 +38,82 @@ test('Should set the right message', () => {
     expect(store.getState().message).toEqual(message);
 });
 
+//helpers
+test('Should call save with the right data', done => {
+    const actions = { setMessage: message => message };
+    const axios = mockAxios;
+
+    save(
+        axios,
+        actions,
+        'get',
+        'http://localhost:9001/tasks'
+    );
+
+    expect(mockAxios.get).toHaveBeenCalled();
+    done();
+});
+
+test('Should call checkProps', done => {
+    const setMessage = jest.fn();
+    const dontSetMessage = jest.fn();
+    const { title, description } = todo;
+
+    checkProps({ dontSetMessage }, title, description);
+    checkProps({ setMessage });
+
+    expect(dontSetMessage).not.toHaveBeenCalled();
+    expect(setMessage).toHaveBeenCalled();
+    done();
+});
+
 //thunks
-test('Should fetch the right data', done => {
+test('Should be called to fetch data', done => {
     const store = createStore(model, { injections : { axios: mockAxios } });
     store.getActions().fetchTodos();
 
     expect(mockAxios.get).toHaveBeenCalled();
+    done();
+});
+
+test('Should be called to add the todo', done => {
+    const store = createStore(model, { injections: { axios: mockAxios } });
+    const { title, description } = todo;
+
+    store.getActions().addTodo({ title, description });
+
+    expect(mockAxios.post).toHaveBeenCalled();
+    expect(mockAxios.post).toHaveBeenCalledWith(
+        `http://localhost:9001/task/create/${title}/${description}`
+    );
+
+    done();
+});
+
+test('Should be called to update the todo', done => {
+    const store = createStore(model, { injections: { axios: mockAxios } });
+    const { id, title, description } = todo;
+
+    store.getActions().updateTodo(todo);
+
+    expect(mockAxios.put).toHaveBeenCalled();
+    expect(mockAxios.put).toHaveBeenCalledWith(
+        `http://localhost:9001/task/update/${id}/${title}/${description}`
+    );
+
+    done();
+});
+
+test('Should be called to delete the todo', done => {
+    const store = createStore(model, { injections: { axios: mockAxios } });
+    const { id } = todo;
+
+    store.getActions().removeTodo(id);
+
+    expect(mockAxios.delete).toHaveBeenCalled();
+    expect(mockAxios.delete).toHaveBeenCalledWith(
+        `http://localhost:9001/task/delete/${id}`
+    );
+
     done();
 });
